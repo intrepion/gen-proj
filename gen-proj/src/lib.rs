511b0b76b35b599a133b18512c8a1a2a4fd83e5e
Oliver Forral <intrepion@gmail.com>
2022-03-05 02:54:56 -0700

    green

--- gen-proj/src/lib.rs
+++ gen-proj/src/lib.rs
@@ -1,8 +1,14 @@
 #[cfg(test)]
 mod make_canonical_should {
+    use super::make_canonical;
+
     #[test]
     fn return_lowercase() {
         let _expected = "hello";
         let _actual = make_canonical("Hello");
     }
+}
+
+pub fn make_canonical(input: &str) -> String {
+    input.to_lowercase()
 }
