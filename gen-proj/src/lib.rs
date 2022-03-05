9edec5553d0e0d07d013d5268bd886ac72be67bd
Oliver Forral <intrepion@gmail.com>
2022-03-05 02:52:50 -0700

    red

--- gen-proj/src/lib.rs
+++ gen-proj/src/lib.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
-mod tests {
+mod make_canonical_should {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn return_lowercase() {
+        let _expected = "hello";
+        let _actual = make_canonical("Hello");
     }
 }
