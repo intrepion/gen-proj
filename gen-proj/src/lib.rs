45a3d7bd1983e29fdd6174546ec3cf7816a598cc
Oliver Forral <intrepion@gmail.com>
2022-03-05 03:00:54 -0700

    red

--- gen-proj/src/lib.rs
+++ gen-proj/src/lib.rs
@@ -10,6 +10,24 @@ mod make_canonical_should {
 
         assert_eq!(actual, expected)
     }
+
+    #[test]
+    fn return_trimmed() {
+        let expected = "hello";
+
+        let actual = make_canonical("  Hello  ");
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn return_hyphen_for_special_characters() {
+        let expected = "three-men-s-morris";
+
+        let actual = make_canonical("Three Men's Morris");
+
+        assert_eq!(actual, expected)
+    }
 }
 
 pub fn make_canonical(_input: &str) -> String {
