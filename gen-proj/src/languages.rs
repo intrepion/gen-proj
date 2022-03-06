f4e3479eafe4cf948db1f384f08dd9997688caa1
Oliver Forral <intrepion@gmail.com>
2022-03-05 17:50:18 -0700

    green

--- gen-proj/src/languages.rs
+++ gen-proj/src/languages.rs
@@ -2,6 +2,7 @@
 mod languages {
     mod from_string_should {
         use super::super::Language;
+        use std::str::FromStr;
 
         #[test]
         fn return_rust_enum_given_rust_string() {
@@ -11,6 +12,16 @@ mod languages {
     }
 }
 
+use std::str;
+
 pub enum Language {
     Rust,
+}
+
+impl str::FromStr for Language {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Err(format!("Unknown language: {}", s))
+    }
 }
