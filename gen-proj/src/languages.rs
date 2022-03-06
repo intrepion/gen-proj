58d63eac17e5eeb7af78b3491bdc7456ed17ad54
Oliver Forral <intrepion@gmail.com>
2022-03-05 17:41:32 -0700

    green

--- gen-proj/src/languages.rs
+++ gen-proj/src/languages.rs
@@ -1,9 +1,15 @@
 #[cfg(test)]
 mod languages {
     mod from_string_should {
+        use super::super::Language;
+
         #[test]
         fn return_rust_enum_given_rust_string() {
             let _expected = Language::Rust;
         }
     }
+}
+
+pub enum Language {
+    Rust,
 }
