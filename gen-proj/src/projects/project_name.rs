df055e2641015d2a5e2603d9bd76161f708c62f9
Oliver Forral <intrepion@gmail.com>
2022-03-06 22:33:01 -0700

    red

--- gen-proj/src/projects/project_name.rs
+++ gen-proj/src/projects/project_name.rs
@@ -6,6 +6,8 @@ mod test_project_name {
         #[test]
         fn return_trimmed_project_name() {
             let _expected = ProjectName("Hello World".to_owned());
+
+            let _actual = ProjectName::new("  Hello World    ".to_owned());
         }
     }
 }
