23c19c5a73975d0d45b13e718de567a013b09c4b
Oliver Forral <intrepion@gmail.com>
2022-03-06 22:10:12 -0700

    green

--- gen-proj/src/projects.rs
+++ gen-proj/src/projects.rs
@@ -1,9 +1,13 @@
 #[cfg(test)]
 mod test_projects {
     mod default_should {
+        use super::super::Project;
+
         #[test]
         fn return_project_with_defaults() {
             let _expected = Project {};
         }
     }
 }
+
+pub struct Project {}
