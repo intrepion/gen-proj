3d35bda450b6990e887fa11022eca9760d6ebc28
Oliver Forral <intrepion@gmail.com>
2022-03-06 22:42:37 -0700

    green

--- gen-proj/src/projects/project_name.rs
+++ gen-proj/src/projects/project_name.rs
@@ -27,7 +27,7 @@ mod test_project_name {
 pub struct ProjectName(String);
 
 impl ProjectName {
-    pub fn new(_raw: &str) -> ProjectName {
-        ProjectName("Hello World".to_owned())
+    pub fn new(raw: &str) -> ProjectName {
+        ProjectName(raw.trim().to_owned())
     }
 }
